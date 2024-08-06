5bc818c71e6926d48b4cc7643342dda352d42d8c
Shamal <shamalgithub@gmail.com>
2024-08-06 16:44:50 +0530

    streaming client added

--- abstract_basellm.py
+++ abstract_basellm.py
@@ -5,6 +5,7 @@ from abc import ABC  , abstractmethod
 class BaseLLMProvider(ABC):
     def __init__(self) -> None: 
         self.client = httpx.AsyncClient(http2=True)
+        self.stream_client = httpx.AsyncClient()
     async def __aenter__(self):
         return self 
     async def __aexit__(self , exc_type , exc_val , exc_tb):
